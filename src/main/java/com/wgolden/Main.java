bdde1d42c280ff416813285bd2c1b062607be53f
= <=>
2024-04-05 06:53:45 -0500

    Add catalog manager singleton

--- src/main/java/com/wgolden/Main.java
+++ src/main/java/com/wgolden/Main.java
@@ -1,10 +1,7 @@
 package com.wgolden;
 
 import com.microsoft.sqlserver.jdbc.*;
-import com.wgolden.ssisdb.SSISExecutionManager;
-import com.wgolden.ssisdb.SSISExecutionBuilder;
-import com.wgolden.ssisdb.SSISExecutionParameter;
-import com.wgolden.ssisdb.SSISExecutionParameterBuilder;
+import com.wgolden.ssisdb.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -36,38 +33,43 @@ public class Main {
         datasource.setTrustServerCertificate(true);
         datasource.setAuthenticationScheme("NTLM");
 
-        SSISExecutionManager executionManager = SSISExecutionManager.getInstance();
-        try {
-            var ssisExecution = new SSISExecutionBuilder()
-                    .dataSource(datasource)
-                    .folderName("projects")
-                    .projectName("myssisproject")
-                    .packageName("Package.dtsx")
-                    .ssisExecutionManager(executionManager)
-                    .build();
 
-            SSISExecutionParameter<Boolean> param = new SSISExecutionParameterBuilder<Boolean>()
-                    .executionId(ssisExecution.getExecutionId())
-                    .objectType(50)
-                    .parameterName("SYNCHRONIZED")
-                    .parameterValue(true)
-                    .dataSource(datasource)
-                    .build();
-
-            SSISExecutionParameter<Integer> logging = new SSISExecutionParameterBuilder<Integer>()
-                    .executionId(ssisExecution.getExecutionId())
-                    .objectType(50)
-                    .parameterName("LOGGING_LEVEL")
-                    .parameterValue(3)
-                    .dataSource(datasource)
-                    .build();
-
-            executionManager.setExecutionParameter(param);
-            executionManager.setExecutionParameter(logging);
-            executionManager.startExecution(ssisExecution);
-
-        } catch (SQLException e) {
+        SSISCatalogManager catalogManager = SSISCatalogManager.getInstance();
+        try{
+            String folderName = "test_folder";
+            catalogManager.deleteFolder(datasource, folderName);
+        } catch (RuntimeException e) {
             throw new RuntimeException(e);
         }
+    }
+    private static void createExecution(SQLServerDataSource dataSource) throws SQLException {
+        SSISExecutionManager executionManager = SSISExecutionManager.getInstance();
+        var ssisExecution = new SSISExecutionBuilder()
+                .dataSource(dataSource)
+                .folderName("projects")
+                .projectName("myssisproject")
+                .packageName("Package.dtsx")
+                .ssisExecutionManager(executionManager)
+                .build();
+
+        SSISExecutionParameter<Boolean> param = new SSISExecutionParameterBuilder<Boolean>()
+                .executionId(ssisExecution.getExecutionId())
+                .objectType(50)
+                .parameterName("SYNCHRONIZED")
+                .parameterValue(true)
+                .dataSource(dataSource)
+                .build();
+
+        SSISExecutionParameter<Integer> logging = new SSISExecutionParameterBuilder<Integer>()
+                .executionId(ssisExecution.getExecutionId())
+                .objectType(50)
+                .parameterName("LOGGING_LEVEL")
+                .parameterValue(3)
+                .dataSource(dataSource)
+                .build();
+
+        executionManager.setExecutionParameter(param);
+        executionManager.setExecutionParameter(logging);
+        executionManager.startExecution(ssisExecution);
     }
 }
---
+++ src/main/java/com/wgolden/ssisdb/SSISCatalogManager.java
@@ -0,0 +1,54 @@
+package com.wgolden.ssisdb;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class SSISCatalogManager {
+
+    private static SSISCatalogManager instance;
+    private SSISCatalogManager(){}
+    public static SSISCatalogManager getInstance(){
+        if(instance == null){
+            instance = new SSISCatalogManager();
+        }
+        return instance;
+    }
+
+    public int createFolder(SQLServerDataSource dataSource, String folderName) throws RuntimeException{
+        String createFolderStmt = """
+                    EXECUTE [catalog].[create_folder]
+                       @folder_name = ?
+                      ,@folder_id = ?
+                """;
+        int folderId = -1;
+        try(Connection conn = dataSource.getConnection();
+            CallableStatement stmt = conn.prepareCall(createFolderStmt);
+        ){
+            stmt.setString(1, folderName);
+            stmt.registerOutParameter(2, folderId);
+            stmt.execute();
+            folderId = stmt.getInt(2);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return folderId;
+    }
+    public void deleteFolder(SQLServerDataSource dataSource, String folderName) throws RuntimeException{
+        String createFolderStmt = """
+                    EXECUTE [catalog].[delete_folder]
+                       @folder_name = ?
+                """;
+        try(Connection conn = dataSource.getConnection();
+            CallableStatement stmt = conn.prepareCall(createFolderStmt);
+        ){
+            stmt.setString(1, folderName);
+            stmt.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
